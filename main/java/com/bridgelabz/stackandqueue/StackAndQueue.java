493094994e67df5d5e1e093fa9b49732fae71899
shindeanant <shindeanant1997@gmail.com>
2021-10-06 16:01:29 +0530

    ADD: Welcome to stack and queue

---
+++ main/java/com/bridgelabz/stackandqueue/StackAndQueue.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.stackandqueue;
+
+public class StackAndQueue 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Stack and Queue Program" );
+    }
+}
